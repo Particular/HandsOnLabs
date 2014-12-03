9767706f93cbd94ad88a3ee64d95ae86c712709a
indualagarsamy <indu.alagarsamy@gmail.com>
2014-12-03 10:44:17 -0800

    Updated PubSub for MSMQ Transport
    Updated the manual to provide latest instructions for NSB v5
    Added the completed solution.

---
+++ Lab103/Completed Solution/FastCars.Events/ClientBecamePreferred.cs
@@ -0,0 +1,10 @@
+﻿namespace FastCars.Events
+{
+    using System;
+    public class ClientBecamePreferred 
+    {
+        public Guid ClientId { get; set; }
+        public DateTime PreferredUntil { get; set; }
+    }
+}
+
