23f5bee8f717cc1a5d3430e23e90b03db3b665bf
indualagarsamy <indu.alagarsamy@gmail.com>
2014-12-03 18:52:13 -0800

    Updated PubSub for SqlTransport
    Added the completed solution for NSB v5
    Updated the lab manual for v5

---
+++ Lab106/Completed Solution/FastCars.Promotions/ClientBecamePreferredHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace FastCars.Promotions
+{
+    using System;
+    using NServiceBus;
+    using FastCars.Events;
+
+    public class ClientBecamePreferredHandler : IHandleMessages<ClientBecamePreferred>
+    {
+        public void Handle(ClientBecamePreferred message)
+        {
+            Console.WriteLine("Client became preferred, send them a new free rental offer");
+        }
+    }
+}
+
---
+++ Lab106/Completed Solution/FastCars.SharedConventions/ConventionExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace FastCars.SharedConventions
+{
+    using NServiceBus;
+    public static class ConventionExtensions
+    {
+        public static void ApplyCustomConventions(this BusConfiguration busConfiguration)
+        {
+            var conventions = busConfiguration.Conventions();
+            conventions.DefiningCommandsAs(t => t.Namespace != null && t.Namespace.StartsWith("FastCars") && t.Namespace.EndsWith("Commands"));
+            conventions.DefiningEventsAs(t => t.Namespace != null && t.Namespace.StartsWith("FastCars") && t.Namespace.EndsWith("Events"));
+            conventions.DefiningMessagesAs(t => t.Namespace != null && t.Namespace.StartsWith("FastCars") && t.Namespace.EndsWith("Messages"));
+            // In a similar fashion you can define conventions for EncrypedProperties, DataBusProperties, ExpressMessages and TimeToBeRecevied            
+        }
+    }
+}
